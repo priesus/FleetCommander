3deb10aad9b8c4226291e4e51c16ce7f6138a1ae
Stefan Pries <stefan_pries@outlook.com>
2015-03-16 20:16:42 +0100

    super simple universe visualization with hard-coded dummy universe

--- src/main/webapp/resources/js/controllers.js
+++ src/main/webapp/resources/js/controllers.js
@@ -1,9 +1,39 @@
 var fleetCommanderApp = angular.module('fleetCommanderApp', [ 'ngAnimate' ]);
 
 fleetCommanderApp.controller('GamesCtrl', function($scope) {
-	$scope.isIngame = false;
 	$scope.runningGame = {
 	  'url' : 'games/123/',
-	  'userId' : 123456789
+	  'userId' : 123456789,
+	  'universe' : {
+		  'planets' : [ {
+		    'x' : 0,
+		    'y' : 0
+		  }, {
+		    'x' : 5,
+		    'y' : 5
+		  }, {
+		    'x' : 5,
+		    'y' : 45
+		  }, {
+		    'x' : 45,
+		    'y' : 5
+		  }, {
+		    'x' : 45,
+		    'y' : 45
+		  }, {
+		    'x' : 25,
+		    'y' : 25
+		  }, {
+		    'x' : 0,
+		    'y' : 99
+		  }, {
+		    'x' : 99,
+		    'y' : 0
+		  }, {
+		    'x' : 99,
+		    'y' : 99
+		  } ]
+	  }
 	};
+	$scope.isIngame = $scope.runningGame.universe !== undefined;
 });
