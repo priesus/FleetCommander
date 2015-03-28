b80bf4082d2a1eda441edbd7ac2e5ed53eca236d
Stefan Pries <stefan_pries@outlook.com>
2015-03-28 19:44:52 +0100

    Finished refactoring: Use $http instead of $resource to access REST service with authentication token

--- src/main/webapp/resources/js/controllers.js
+++ src/main/webapp/resources/js/controllers.js
@@ -1,96 +1,88 @@
 var fleetCommanderApp = angular.module('fleetCommanderApp', [ 'fleetCommanderServices', 'ngAnimate', 'ngCookies' ]);
 
-fleetCommanderApp.controller('GamesCtrl', [ '$scope', '$cookies', 'GameService', 'GameService_', 'TurnService',
-    'ShipService', function($scope, $cookies, GameService, GameService_, TurnService, ShipService) {
+fleetCommanderApp.controller('GamesCtrl', [
+		'$scope',
+		'$cookies',
+		'GameService',
+		'TurnService',
+		'ShipService',
+		function($scope, $cookies, GameService, TurnService, ShipService) {
 
-	    $scope.isIngame = false;
-	    $scope.showPlanetMenu = false;
-	    $scope.destinationSelectionActive = false;
-	    $scope.blockingActionInProgress = false;
+			$scope.isIngame = false;
+			$scope.showPlanetMenu = false;
+			$scope.destinationSelectionActive = false;
+			$scope.blockingActionInProgress = false;
 
-	    $scope.hasActiveGame = function() {
-		    return $cookies.runningGameId !== undefined;
-	    };
+			$scope.hasActiveGame = function() {
+				return $cookies.runningGameId !== undefined;
+			};
 
-	    $scope.startGame = function() {
+			$scope.startGame = function() {
 
-		    GameService_.create().success(function(data) {
-			    $scope.runningGameId = data.gameId;
-			    $scope.runningGameToken = data.gameAuthToken;
-			    $cookies.runningGameId = $scope.runningGameId;
-			    $cookies.runningGameToken = $scope.runningGameToken;
+				GameService.create().success(function(data) {
+					$cookies.runningGameId = data.gameId;
+					$cookies.runningGameToken = data.gameAuthToken;
+					$scope.resumeGame();
+				});
+			};
 
-			    GameService_.get($scope.runningGameId, $scope.runningGameToken).success(function(data) {
-				    $scope.runningGame = data;
-				    $scope.isIngame = true;
-			    });
-		    });
-	    };
+			$scope.resumeGame = function() {
+				$scope.runningGameId = $cookies.runningGameId;
+				$scope.runningGameToken = $cookies.runningGameToken;
+				$scope.isIngame = true;
+				$scope.reloadGame();
+			};
 
-	    $scope.resumeGame = function() {
-		    $scope.runningGame = GameService.get({
-			    gameId : $cookies.runningGameId
-		    }, function() {
-			    $scope.isIngame = true;
-		    });
-	    };
+			$scope.reloadGame = function() {
+				GameService.get($scope.runningGameId, $scope.runningGameToken).success(function(data) {
+					$scope.runningGame = data;
+				});
+			};
 
-	    $scope.endTurn = function() {
-		    $scope.blockingActionInProgress = true;
-		    TurnService.endTurn({
-			    gameId : $scope.runningGame.id
-		    }, function() {
-			    $scope.reloadGame();
-			    $scope.blockingActionInProgress = false;
-		    }, function() {
-			    $scope.blockingActionInProgress = false;
-		    });
-	    };
+			$scope.endTurn = function() {
+				$scope.blockingActionInProgress = true;
+				TurnService.endTurn($scope.runningGameId, $scope.runningGameToken).success(function() {
+					$scope.reloadGame();
+					$scope.blockingActionInProgress = false;
+				}).error(function() {
+					$scope.blockingActionInProgress = false;
+				});
+			};
 
-	    $scope.quitGame = function() {
-		    $scope.runningGame.$delete();
-		    $scope.isIngame = false;
-		    delete $scope.runningGame;
-		    delete $cookies.runningGameId;
-	    };
+			$scope.quitGame = function() {
+				GameService.quit($scope.runningGameId, $scope.runningGameToken);
+				$scope.isIngame = false;
+				delete $scope.runningGame;
+				delete $cookies.runningGameId;
+			};
 
-	    $scope.clickPlanetHandler = function(planet) {
-		    if (!$scope.destinationSelectionActive && planet.inhabited) {
-			    // Open planet menu
-			    $scope.selectedOriginPlanet = planet;
-			    $scope.showPlanetMenu = true;
-			    $scope.setShipCount(1);
+			$scope.clickPlanetHandler = function(planet) {
+				if (!$scope.destinationSelectionActive && planet.inhabited) {
+					// Open planet menu
+					$scope.selectedOriginPlanet = planet;
+					$scope.showPlanetMenu = true;
+					$scope.setShipCount(1);
 
-		    } else if ($scope.destinationSelectionActive) {
-			    // Send ships from previously selected planet to this planet
-			    ShipService.sendShips({
-			      gameId : $scope.runningGame.id,
-			      shipCount : $scope.shipCount,
-			      originPlanet : $scope.selectedOriginPlanet.id,
-			      destinationPlanet : planet.id
-			    }, function() {
-				    $scope.reloadGame();
-			    });
-			    $scope.destinationSelectionActive = false;
-		    }
-	    };
+				} else if ($scope.destinationSelectionActive) {
+					// Send ships from previously selected planet to this planet
+					ShipService.sendShips($scope.runningGameId, $scope.runningGameToken, $scope.shipCount,
+							$scope.selectedOriginPlanet.id, planet.id).success(function() {
+						$scope.reloadGame();
+					});
+					$scope.destinationSelectionActive = false;
+				}
+			};
 
-	    $scope.setShipCount = function(ships) {
-		    if (ships < 0)
-			    ships = 0;
-		    else if (ships > $scope.selectedOriginPlanet.shipCount)
-			    ships = $scope.selectedOriginPlanet.shipCount
-		    $scope.shipCount = ships;
-	    };
+			$scope.setShipCount = function(ships) {
+				if (ships < 0)
+					ships = 0;
+				else if (ships > $scope.selectedOriginPlanet.shipCount)
+					ships = $scope.selectedOriginPlanet.shipCount
+				$scope.shipCount = ships;
+			};
 
-	    $scope.shipDestinationSelection = function() {
-		    $scope.showPlanetMenu = false;
-		    $scope.destinationSelectionActive = true;
-	    };
-
-	    $scope.reloadGame = function() {
-		    $scope.runningGame = GameService.get({
-			    gameId : $cookies.runningGameId
-		    });
-	    };
-    } ]);
+			$scope.shipDestinationSelection = function() {
+				$scope.showPlanetMenu = false;
+				$scope.destinationSelectionActive = true;
+			};
+		} ]);
--- src/main/webapp/resources/js/services.js
+++ src/main/webapp/resources/js/services.js
@@ -1,82 +1,58 @@
 var fleetCommanderServices = angular.module('fleetCommanderServices', [ 'ngResource' ]);
 
-fleetCommanderServices.factory('GameService_', [ '$http', function($http) {
+fleetCommanderServices.factory('GameService', [ '$http', function($http) {
 	return {
-	  create : function() {
-		  return $http({
-		    method : 'POST',
-		    url : 'rest/games'
-		  });
-	  },
-	  get : function(gameId, token) {
-		  return $http({
-		    method : 'GET',
-		    url : 'rest/games/' + gameId,
-		    headers : {
-			    'Authorization' : 'Bearer ' + token
-		    }
-		  });
-	  }
+		create : function() {
+			return $http({
+				method : 'POST',
+				url : 'rest/games'
+			});
+		},
+		get : function(gameId, token) {
+			return $http({
+				method : 'GET',
+				url : 'rest/games/' + gameId,
+				headers : {
+					'Authorization' : 'Bearer ' + token
+				}
+			});
+		},
+		quit : function(gameId, token) {
+			return $http({
+				method : 'DELETE',
+				url : 'rest/games/' + gameId,
+				headers : {
+					'Authorization' : 'Bearer ' + token
+				}
+			});
+		}
 	};
 } ]);
 
-fleetCommanderServices.factory('TurnService_', [ '$http', function($http) {
+fleetCommanderServices.factory('TurnService', [ '$http', function($http) {
 	return {
 		endTurn : function(gameId, token) {
 			return $http({
-			  method : 'POST',
-			  url : 'rest/games/' + gameId + '/turns',
-			  headers : {
-				  'Authorization' : 'Bearer ' + token
-			  }
+				method : 'POST',
+				url : 'rest/games/' + gameId + '/turns',
+				headers : {
+					'Authorization' : 'Bearer ' + token
+				}
 			});
 		}
 	};
 } ]);
 
-fleetCommanderServices.factory('ShipService_', [ '$http', function($http) {
+fleetCommanderServices.factory('ShipService', [ '$http', function($http) {
 	return {
 		sendShips : function(gameId, token, ships, origin, dest) {
 			return $http({
-			  method : 'POST',
-			  url : 'rest/games/' + gameId + '/universe/travellingShipFormations/' + ships + '/' + origin + '/' + dest,
-			  headers : {
-				  'Authorization' : 'Bearer ' + token
-			  }
+				method : 'POST',
+				url : 'rest/games/' + gameId + '/universe/travellingShipFormations/' + ships + '/' + origin + '/' + dest,
+				headers : {
+					'Authorization' : 'Bearer ' + token
+				}
 			});
 		}
 	};
-} ]);
-
-fleetCommanderServices.factory('GameService', [ '$resource', function($resource) {
-	return $resource('rest/games/:gameId', {
-		gameId : '@id'
-	}, {
-		start : {
-			method : 'POST'
-		}
-	});
-} ]);
-
-fleetCommanderServices.factory('TurnService', [ '$resource', function($resource) {
-	return $resource('rest/games/:gameId/turns', {
-		gameId : '@gameId'
-	}, {
-		endTurn : {
-			method : 'POST'
-		}
-	});
-} ]);
-
-fleetCommanderServices.factory('ShipService', [ '$resource', function($resource) {
-	return $resource('rest/games/:gameId/universe/travellingShipFormations/:ships/:origin/:dest', {
-	  gameId : '@gameId',
-	  ships : '@shipCount',
-	  origin : '@originPlanet',
-	  dest : '@destinationPlanet'
-	}, {
-		sendShips : {
-			method : 'POST'
-		}
-	});
 } ]);
