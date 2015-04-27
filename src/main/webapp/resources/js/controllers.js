e49a4ce5b4edcbb1c0b862201bc17616937b3abd
Stefan Pries <stefan_pries@outlook.com>
2015-04-27 22:07:21 +0200

    Keyboard shortcuts

--- src/main/webapp/resources/js/controllers.js
+++ src/main/webapp/resources/js/controllers.js
@@ -21,6 +21,65 @@ fleetCommanderApp.controller('GamesCtrl', [
 				return $cookies.gameId !== undefined;
 			};
 
+			// TODO refactor to make this more readable
+			$scope.onKeyDown = function($event) {
+				switch ($event.keyCode) {
+				case 83:
+					if ($scope.gameScreen == 'home')
+						// [S]tart new game
+						$scope.createNewGame();
+					else if ($scope.gameScreen == 'ingame')
+						// [s]end ships
+						$scope.prepareSendShips();
+					break;
+				case 82: // [R]esume game
+					$scope.resumeGame();
+					break;
+				case 80: // [P]lay
+					$scope.startGame();
+					break;
+				case 65:
+					if ($scope.gameScreen == 'players')
+						// [A]dd computer player
+						$scope.addComputerPlayer();
+					else if ($scope.gameScreen == 'ingame')
+						// [a]ll ships
+						$scope.setShipCount($scope.selectedPlanet.shipCount);
+					break;
+				case 69: // [E]nd turn
+					$scope.endTurn();
+					break;
+				case 70: // Build [f]actory
+					$scope.buildFactoryOnSelectedPlanet();
+					break;
+				case 189: // [-] ships
+					$scope.setShipCount(shipCount - 1);
+					break;
+				case 187: // [+] ships
+					$scope.setShipCount(shipCount + 1);
+					break;
+				case 13: // [Enter]
+				case 32: // [Space]
+					if ($scope.gameScreen == 'players')
+						// Start game
+						$scope.startGame();
+					else if ($scope.gameScreen == 'ingame')
+						// Close new turn menu
+						$scope.showTurnEvents = false;
+					break;
+				case 27: // [Esc]
+					if ($scope.gameScreen == 'players')
+						// Cancel game creation
+						$scope.quitGame();
+					else if ($scope.gameScreen == 'ingame') {
+						// Close planet menu/new turn menu
+						$scope.showTurnEvents = false;
+						$scope.showPlanetMenu = false;
+					}
+					break;
+				}
+			};
+
 			$scope.createNewGame = function() {
 				GamesService.create().success(function(data) {
 					$scope.gameId = data.gameId;
@@ -40,6 +99,13 @@ fleetCommanderApp.controller('GamesCtrl', [
 				GamesService.start($scope.gameId, $scope.gameToken).success(function() {
 					$cookies.gameId = $scope.gameId;
 					$cookies.gameToken = $scope.gameToken;
+
+					// TODO shouls we extract some methods for setting the variables?
+					$scope.showPlanetMenu = false;
+					$scope.showTurnEvents = false;
+					$scope.destinationSelectionActive = false;
+					$scope.blockingActionInProgress = false;
+
 					$scope.resumeGame();
 				});
 			};
@@ -59,6 +125,9 @@ fleetCommanderApp.controller('GamesCtrl', [
 			};
 
 			$scope.endTurn = function() {
+				if ($scope.blockingActionInProgress || $scope.game.status == 'OVER')
+					return;
+
 				$scope.blockingActionInProgress = true;
 				TurnsService.endTurn($scope.gameId, $scope.gameToken).success(function() {
 					$scope.reloadGame();
@@ -102,22 +171,24 @@ fleetCommanderApp.controller('GamesCtrl', [
 			$scope.setShipCount = function(ships) {
 				if (ships < 1)
 					ships = 1;
-				else if (ships > $scope.selectedPlanet.shipCount)
+				if (ships > $scope.selectedPlanet.shipCount)
 					ships = $scope.selectedPlanet.shipCount
 				$scope.shipCount = ships;
 			};
 
-			$scope.buildFactory = function(planet) {
-				PlanetsService.buildFactory($scope.gameId, $scope.gameToken, planet.id).success(function() {
+			$scope.buildFactoryOnSelectedPlanet = function() {
+				PlanetsService.buildFactory($scope.gameId, $scope.gameToken, $scope.selectedPlanet.id).success(function() {
 					$scope.selectedPlanet.factorySite.factoryCount++;
 					$scope.selectedPlanet.factorySite.availableSlots--;
 					$scope.reloadGame();
 				});
 			};
 
-			$scope.shipDestinationSelection = function() {
-				$scope.showPlanetMenu = false;
-				$scope.destinationSelectionActive = true;
+			$scope.prepareSendShips = function() {
+				if ($scope.shipCount > 0) {
+					$scope.showPlanetMenu = false;
+					$scope.destinationSelectionActive = true;
+				}
 			};
 
 			$scope.getNumber = function(num) {
